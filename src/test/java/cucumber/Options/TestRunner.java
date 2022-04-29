04c616c4b4f11120f1b6621f5b58602ccc48574e
Yogesh <yogesh.lolage@aress.com>
2022-04-29 12:34:45 +0530

    pom updated

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -1,8 +1,19 @@
 package cucumber.Options;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+//import static io.cucumber.listener.Reporter;
+
+import resources.FileReaderManager;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/features",
@@ -10,8 +21,15 @@ import io.cucumber.junit.CucumberOptions;
 				glue= {"stepDefination"},
 				dryRun = false,
 				monochrome = true,
-				tags = "@Regression"
+				tags = "@withoutParameter"
 				)
+
 public class TestRunner {
 	
+	@AfterClass
+	 public static void writeExtentReport() throws FileNotFoundException, IOException 
+	{
+		
+		 
+	 }
 }
